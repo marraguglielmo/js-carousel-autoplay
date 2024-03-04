1c472da9dc40209daa425668e53340d122aaa207
Guglielmo Marra <156308467+marraguglielmo@users.noreply.github.com>
2024-03-04 14:51:07 +0100

    [add] readme, function click

---
+++ JS/script.js
@@ -0,0 +1,64 @@
+// elements
+const imgWrapper = document.querySelector('.img-wrapper');
+const btnTop = document.querySelector(".top")
+const btnBottom = document.querySelector(".bottom");
+// 4. inizializzo il counter
+let counter = 0;
+
+// 1.
+const images = [
+    'img/01.webp',
+    'img/02.webp',
+    'img/03.webp',
+    'img/04.webp',
+    'img/05.webp',
+];
+
+//2.
+for(let i = 0; i < images.length; i++){
+
+    imgWrapper.innerHTML += `<img class="img d-none" src="${images[i]}">`
+    
+}
+
+// 3.  prendo tutti gli elementi con la classe img
+const imgCollection = document.getElementsByClassName("img");
+
+// rimuovo la classe d-none alla prima immagine
+imgCollection[counter].classList.remove("d-none");
+
+// 8.
+    btnTop.classList.add("d-none");
+
+
+
+
+// quando sono in hover sullo slider disattivo setInterval e uso i bottoni /////
+    
+// sopra decremento
+btnTop.addEventListener('click', pressTop);
+
+// sotto incremento
+btnBottom.addEventListener('click', pressBottom);
+
+
+// FUNCTIONS //////
+function pressBottom(){
+    imgCollection[counter++].classList.add("d-none");
+    imgCollection[counter].classList.remove("d-none");
+
+    btnTop.classList.remove("d-none");
+    if(counter === images.length - 1){
+        btnBottom.classList.add("d-none");
+    }
+}
+
+function pressTop(){
+    imgCollection[counter--].classList.add("d-none");
+    imgCollection[counter].classList.remove("d-none");
+
+    btnBottom.classList.remove("d-none");
+    if(counter === 0){
+        btnTop.classList.add("d-none");
+    }
+}
---
+++ README.md
@@ -0,0 +1,12 @@
+# Carousel Autoplay
+
+Riprendendo il carousel realizzato in passato e con le Timeng Function rendete lo scroll automatico ogni 3 secondi.
+Dopo l’ultima slide deve riapparire la prima e così via
+
+#### **BONUS**
+
+Per evitare conflitti con lo scroll manuale, quando il mouse si trova sopra lo slider lo scroll automatico si interrompe per poi ripartire all’uscita del mouse
+
+## Svolgimento
+
+1. inserisco i click per scorrere avanti e indietro in due funzioni
